5906694dc6181b0f72fe5f376d96547896c1a0dc
Tom Daniel <tomudoh258@gmail.com>
2023-07-11 18:09:29 +0100

    Add test for music_album and genre class

---
+++ spec/music_album_spec.rb
@@ -0,0 +1,35 @@
+require './music_album'
+
+RSpec.describe MusicAlbum do
+  let(:release_date) { Date.new(2023, 1, 1) }
+  let(:album) { MusicAlbum.new('Title', 'Artist', release_date, 'Label') }
+
+  describe '#can_be_archived?' do
+    # context 'when on_spotify is true and can_be_archived? from the parent class returns true' do
+    #   it 'returns true' do
+    #     album.on_spotify = true
+    #     allow(album).to receive(:super_can_be_archived?).and_return(true)
+
+    #     expect(album.can_be_archived?).to eq(true)
+    #   end
+    # end
+
+    context 'when on_spotify is false' do
+      it 'returns false' do
+        album.on_spotify = false
+        allow(album).to receive(:super_can_be_archived?).and_return(true)
+
+        expect(album.can_be_archived?).to eq(false)
+      end
+    end
+
+    context 'when can_be_archived? from the parent class returns false' do
+      it 'returns false' do
+        album.on_spotify = true
+        allow(album).to receive(:super_can_be_archived?).and_return(false)
+
+        expect(album.can_be_archived?).to eq(false)
+      end
+    end
+  end
+end
