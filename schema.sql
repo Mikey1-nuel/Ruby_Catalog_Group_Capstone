59ee6980de2b16d5b48daa260c985039eef5583f
Tom Daniel <tomudoh258@gmail.com>
2023-07-11 23:03:55 +0100

    Create database with associations between music_album and genre

--- schema.sql
+++ schema.sql
@@ -2,7 +2,7 @@ CREATE DATABASE CATALOG;
 
 CREATE TABLE books (
     id Int generated always as identity,
-    publisher  varchar(255) ,
+    publisher  varchar(255),
     cover_state  varchar(255),
     publish_date  date,
     archived  boolean,
@@ -15,5 +15,38 @@ CREATE TABLE labels (
     id int generated always as identity,
     title varchar(255),
     color varchar(255),
+    primary key (id)
+);
+
+CREATE TABLE music_albums (
+    id Int generated always as identity,
+    title  varchar(255),
+    artist  varchar(255),
+    release_date  date,
+    label  varchar(255),
+    on_spotify  boolean,
+    archived  boolean,
+    label_id  Int,
+    CONSTRAINT fk_label FOREIGN KEY (label_id) REFERENCES labels(id),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE genres (
+    id int generated always as identity,
+    name varchar(255),
+    primary key (id)
+);
+
+CREATE TABLE items (
+    id Int generated always as identity,
+    genre varchar(255),
+    type varchar(255),
+    publisher varchar(255),
+    cover_state varchar(255),
+    publish_date date,
+    title varchar(255),
+    artist varchar(255),
+    release_date date,
+    label varchar(255),
     primary key (id)
 );
